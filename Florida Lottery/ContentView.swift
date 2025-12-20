adb4090c6cd725f98129edfb33ac60655d330627
Tyler Hartman <tyhartman@outlook.com>
2025-12-19 23:42:20 -0500

    Initial Commit

---
+++ Florida Lottery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Florida Lottery
+//
+//  Created by Tyler Hartman on 12/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Florida Lottery/Florida_LotteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Florida_LotteryApp.swift
+//  Florida Lottery
+//
+//  Created by Tyler Hartman on 12/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Florida_LotteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
